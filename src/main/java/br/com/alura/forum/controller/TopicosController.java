f3c7eda1bc289d8f9fe2761dea5415491d3b13a0
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-05-23 17:03:55 -0300

    criando repository e a busca customizada

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @Autowired
+    TopicoRepository topicoRepository;
+
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(String nomeCurso) {
+
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+
+    }
+}
