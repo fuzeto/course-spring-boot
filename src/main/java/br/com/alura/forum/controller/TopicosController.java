f85569ba811dcc5f0aea3df6af5e2326c1d75359
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-05-23 18:49:17 -0300

    fluxo de update de um item

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.dto.TopicoDto;
@@ -8,6 +9,7 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -51,5 +53,17 @@ public class TopicosController {
     public DetalhesDoTopicoDto detalhar(@PathVariable("id") Long id) {
         Topico topico = topicoRepository.getOne(id);
         return new DetalhesDoTopicoDto(topico);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable("id") Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+
+        Topico topico = form.atualizar(id, topicoRepository);
+
+        // não precisa chamar o repository para salvar, pois como estamos numa transação, o JPA identifica que houve
+        // alteração na entidade e já persiste.
+
+        return ResponseEntity.ok(new TopicoDto(topico));
     }
 }
