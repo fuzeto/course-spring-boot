73ea1fe605a33080a3d19c31086a2f4677688830
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-05-27 19:41:22 -0300

    adicionando regras de segurança com sprig security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2y$12$9EfsGNScQPmyjTud/MvhSOts4Vx.58Coym8rlZOLHITBrzUl/zN/m');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
